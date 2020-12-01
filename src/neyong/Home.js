177246482f0eadf39d7bcd28ccc10e251fa76068
utolee90 <itdatabasewiki@gmail.com>
2020-12-02 01:23:21 +0900

    버튼 추가

--- src/neyong/Home.js
+++ src/neyong/Home.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import 'pages/main.css'
 import species from 'images/species-removebg-preview.png'
-
+import {Button} from 'antd';
+import {Link} from 'react-router-dom';
+import {TrophyFilled} from '@ant-design/icons';
 
 
 function Home(){
+    const menuselect = (e) => {
+        document.querySelectorAll('.menu').forEach(
+            v=> {v.innerHTML==='승률계산'?v.classList.add('active'):v.classList.remove('active')}
+        ); //active 모두 제거
+    }
 
     return(
         <div className='home'>
                 <div>
-                    <h1>WhosTherWinner에 오신걸 환영합니다!</h1>
+                    <h1>WhosTheWinner에 오신걸 환영합니다!</h1>
                 </div>
                 <div style={{'margin-top':'25px'}}>
                     <img  className= 'specie' src={species} width='40%'/>
@@ -21,7 +28,14 @@ function Home(){
                             이번게임의 승률을 알아 볼 수 있습니다!
                         </p>
                     </div>
-                </div>    
+                </div> 
+                <Link to="winlose" onClick={menuselect}>
+                <Button className='button-to-match'
+                style ={{fontSize: '2em', fontWeight:600, height:'2.5em', backgroundColor:'rgba(192,192,224,0.5)',
+                    marginTop:'30px', borderRadius:'10px', color:'whitesmoke'}}>
+                    <TrophyFilled style={{fontSize:'1.8em'}} /> 승률 예측하기
+                </Button>
+                </Link>
         </div>
     );
 };
